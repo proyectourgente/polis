89f142df4fdc7669784eb27252cffd09c3cc8725
raúl <rsperoni@magnesium.coop>
2021-11-27 14:57:43 -0300

    enunciados fijos

--- server/src/config.js
+++ server/src/config.js
@@ -32,7 +32,7 @@ function get(key) {
 function isDevMode() {
   return devMode;
 }
-let enunciadosfijos = [1]
+let enunciadosfijos = [0]
 module.exports = {
   domainOverride,
   getServerNameWithProtocol,
